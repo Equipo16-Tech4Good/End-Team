a14c9bcd29f1b5d94062a76c4dacc3f3cc9c8891
IsmaelBaena <ismael.baena.contacto@gmail.com>
2023-06-21 00:32:30 +0200

    Sql query modificada

--- SQL_Query.sql
+++ SQL_Query.sql
@@ -4,18 +4,18 @@ insert into NivelesMedallas values
 	('Bronce', 'urlTest');
 
 insert into Usuarios values
-	('email@prueba.com', 'Pruebini Gonzalez', '1234', 0, 0, '2023-06-20 13:27:59.1430000');
+	('rigobertoMolon@gmail.com', 'Rigoberto Molon', '1234', 0, 0);
 
 insert into Medallas values
-	('Medalla Oro', 'Una medalla estupenda', '2023-06-20 13:27:59.1430000', 5, 4),
-	('Medalla Oro', 'Una medalla estupenda', '2023-06-20 13:27:59.1430000', 5, 4),
-	('Medalla Oro', 'Una medalla estupenda', '2023-06-20 13:27:59.1430000', 5, 4),
-	('Medalla Oro', 'Una medalla estupenda', '2023-06-20 13:27:59.1430000', 5, 4),
-	('Medalla Plata', 'Una medalla estupenda', '2023-06-20 13:27:59.1430000', 5, 5),
-	('Medalla Plata', 'Una medalla estupenda', '2023-06-20 13:27:59.1430000', 5, 5),
-	('Medalla Plata', 'Una medalla estupenda', '2023-06-20 13:27:59.1430000', 5, 5),
-	('Medalla Bronce', 'Una medalla estupenda', '2023-06-20 13:27:59.1430000', 5, 6),
-	('Medalla Bronce', 'Una medalla estupenda', '2023-06-20 13:27:59.1430000', 5, 6);
+	(1, 1),
+	(1, 1),
+	(1, 1),
+	(1, 1),
+	(1, 2),
+	(1, 2),
+	(1, 2),
+	(1, 3),
+	(1, 3);
 
 insert into Logros values
 	('Un logro espectacular', 'un superlogro', 'img ejemplo'),
@@ -23,16 +23,29 @@ insert into Logros values
 	('Un logro superguachi', 'un superlogro', 'img ejemplo');
 
 insert into UsuariosLogros values
-	('2023-06-20 13:27:59.1430000', 5, 1),
-	('2023-06-20 13:27:59.1430000', 5, 2),
-	('2023-06-20 13:27:59.1430000', 5, 3);
+	(5, 1),
+	(5, 2),
+	(5, 3);
 
 insert into Mensajes values
-	('Supermensaje 1', 'lolo'),
-	('Supermensaje 2', 'lolo'),
-	('Supermensaje 3', 'lolo'),
-	('Supermensaje 4', 'lolo'),
-	('Supermensaje 5', 'lolo'),
-	('Supermensaje 6', 'lolo'),
-	('Supermensaje 7', 'lolo'),
-	('Supermensaje 8', 'lolo');
+	('Sabias que', 'Octubre es el mes en el que cae la mayor cantidad de lluvia.'),
+	('Sabias que', 'Después de octubre, los meses con mucha lluvia son septiembre y noviembre.'),
+	('Sabias que', 'Julio es el mes en el que llueve menos, seguido de junio y febrero.'),
+	('Sabias que', 'En enero, caen aproximadamente 38 litros de agua. Eso es como llenar una pequeña piscina inflable para que puedas chapotear y jugar'),
+	('Sabias que', 'En febrero, caen alrededor de 35 litros de agua. Eso sería como llenar un cubo gigante de agua para hacer burbujas y juegos acuáticos.'),
+	('Sabias que', 'En marzo, caen aproximadamente 49 litros de agua. Eso es como tener un día de lluvia perfecto para saltar en los charcos con botas de agua.'),
+	('Sabias que', 'En abril, caen alrededor de 54 litros de agua. Eso sería como llenar una cubeta grande para regar muchas plantas y hacer que crezcan sanas.'),
+	('Sabias que', 'En mayo, caen aproximadamente 54 litros de agua. Eso es como llenar una piscina pequeña donde puedas nadar y disfrutar del agua fresca. '),
+	('Sabias que', 'En junio, caen alrededor de 37 litros de agua. Eso sería como tener una divertida lluvia de verano para correr y reír bajo ella.'),
+	('Sabias que', 'En julio, caen aproximadamente 26 litros de agua. Eso es como llenar un cubo con agua para refrescarte y mojarte en un día caluroso.'),
+	('Sabias que', 'En agosto, caen alrededor de 39 litros de agua. Eso sería como llenar una regadera grande para regar un jardín y ver cómo las flores crecen hermosas.'),
+	('Sabias que', 'En septiembre, caen aproximadamente 79 litros de agua. Eso es como tener un día de lluvia muy especial para saltar en los charcos y hacer barquitos de papel.'),
+	('Sabias que', 'En octubre, caen alrededor de 79 litros de agua. Eso sería como llenar un depósito de agua portátil para acampar o ir de excursión.'),
+	('Sabias que', 'En noviembre, caen aproximadamente 58 litros de agua. Eso es como llenar una bañera grande.'),
+	('Sabias que', ' En diciembre, caen alrededor de 43 litros de agua. Eso sería como llenar un rociador de agua para crear un arcoíris de agua y refrescarte al jugar en el jardín'),
+	('Sabias que', 'Sabes que Octubre es el mes que más llueve'),
+	('Sabias que', 'Los meses más inestables, llenos de sorpresas en el clima, son octubre, noviembre y diciembre'),
+	('Sabias que', 'Sabes que en algunos años no ha llovido durante un mes entero'),
+	('Sabias que', 'Sabes que 1971 fue el año que más llovió'),
+	('Sabias que', 'Sabes que 1901 fue el segundo año que más llovió'),
+	('Sabias que', 'Sabes que 2018 fue el tercer año que más llovió desde que tuvimos registros');
